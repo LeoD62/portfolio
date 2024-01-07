dba710f8b2fd2a0f2e30c501bc83bb65eb0cb9c6
Léo Denis <leo.denis622@gmail.com>
2024-01-07 15:49:26 +0100

    :bug: correction deploiment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://LeoD62.github.io',
+    site: 'https://LeoD62.github.io/portfolio',
 });
